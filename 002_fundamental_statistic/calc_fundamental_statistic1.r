839e3196f7f85d72680d7f78bdaea6d680f7cc2a
kojiro629 <kojiro629@gmail.com>
2020-08-16 19:05:48 +0900

    Update comments of 'calc_fundamental_statistic1.r'. #2

--- 002_fundamental_statistic/calc_fundamental_statistic1.r
+++ 002_fundamental_statistic/calc_fundamental_statistic1.r
@@ -1,28 +1,28 @@
 # leaf_size.csvを読み込んで、"leaf_size"という名前のデータフレームに格納する。
 leaf_size <- read.table("./leaf_size.csv", header=TRUE, encoding="UTF-8", sep=",")
 
-# 東斜面と西斜面の平均値
+# 基本統計量を求める各関数を使って、複数の列（東斜面と西斜面）の基本統計量を求める。
+
+# 平均値
 mean(leaf_size$east)
 mean(leaf_size$west)
 
-# 東斜面と西斜面の最小値
+# 最小値
 min(leaf_size$east)
 min(leaf_size$west)
 
-# 東斜面と西斜面の最大値
+# 最大値
 max(leaf_size$east)
 max(leaf_size$west)
 
-# 東斜面と西斜面の中央値
+# 中央値
 median(leaf_size$east)
 median(leaf_size$west)
 
-# 東斜面と西斜面の標準偏差
+# 標準偏差
 sd(leaf_size$east)
 sd(leaf_size$west)
 
-# 東斜面と西斜面の分散
+# 分散
 var(leaf_size$east)
 var(leaf_size$west)
-
-
