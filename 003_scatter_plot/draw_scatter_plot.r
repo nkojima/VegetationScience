f0568a1f0484c93a2a10a56e0024bb38cf2c045b
kojiro629 <kojiro629@gmail.com>
2020-08-16 23:06:34 +0900

    Update comments of 'draw_scatter_plot.r' #3

--- 003_scatter_plot/draw_scatter_plot.r
+++ 003_scatter_plot/draw_scatter_plot.r
@@ -1,3 +1,5 @@
+# tree_height.csvを読み込む。
 tree_height <- read.table("./tree_height.csv", header=TRUE, encoding="UTF-8", sep=",")
 
+# 標高と樹高との相関係数を散布図として描く。
 plot(tree_height$altitude, tree_height$tree_height)
