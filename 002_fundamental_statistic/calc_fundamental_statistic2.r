26e3b6a05c865eb3c7af67cf11e57be5edbda7c4
kojiro629 <kojiro629@gmail.com>
2020-08-16 19:06:04 +0900

    Update comments of 'calc_fundamental_statistic2.r'. #2

--- 002_fundamental_statistic/calc_fundamental_statistic2.r
+++ 002_fundamental_statistic/calc_fundamental_statistic2.r
@@ -2,9 +2,21 @@
 leaf_size <- read.table("./leaf_size.csv", header=TRUE, encoding="UTF-8", sep=",")
 
 # sapply関数を使って、複数の列（東斜面と西斜面）の基本統計量を求める。
+
+# 平均値
 sapply(leaf_size, mean)
+
+# 最小値
 sapply(leaf_size, min)
+
+# 最大値
 sapply(leaf_size, max)
+
+# 中央値
 sapply(leaf_size, median)
+
+# 標準偏差
 sapply(leaf_size, sd)
+
+# 分散
 sapply(leaf_size, var)
